564c0ac83a442d8db8eae05feb4c70d0e2aae18d
masterarun <littlemaster.arun@gmail.com>
2015-09-03 00:02:48 +0530

    map changes

---
+++ UserProfiler/Controllers/GoogleMapController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserProfiler.Controllers
+{
+    public class GoogleMapController : Controller
+    {
+        //
+        // GET: /GoogleMap/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
